a8533956deaa46f583a454b7d3323b83d1f34af0
Zimovchik <z4haroff.ivan@yandex.com>
2022-02-09 18:28:19 +0300

    2 задача

--- WEB/main.py
+++ WEB/main.py
@@ -9,4 +9,10 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route("/training/<prof>")
+def training(prof):
+    print("инженер" in prof or "строитель" in prof)
+    return render_template("base.html", t=prof)
+
+
 app.run(port=8080, host='127.0.0.1')
